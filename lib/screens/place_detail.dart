66f1c61bc1aa8cc80815a4e5d5e7a06c465bf94a
Zeeman C Deka <79737506+Zeeman-C-Deka@users.noreply.github.com>
2023-07-09 18:21:23 +0530

    Update place_detail.dart

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -10,7 +10,7 @@ class PlaceDetailScreen extends StatelessWidget {
   String get locationImage {
     final lat = place.location.latitude;
     final lng = place.location.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyCVUI-4maMo3yY91NH4MsT-cVGaL0VrOb4';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key= Your Key';
   }
 
   @override
